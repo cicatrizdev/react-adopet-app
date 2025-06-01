9d60735cc00a550f0b9142cf66dbe00c5263adb6
Pedro Mello <contato@cicatriz.dev>
2025-06-01 19:20:14 -0300

    commit aula 5.2

--- global.d.ts
+++ global.d.ts
@@ -15,4 +15,7 @@ declare module '*.css' {
 
 declare module 'simple-auth-storage' {
 	export const generateToken: () => string;
+	export const saveUser: (user: User, token: string) => void;
+	export const getUser: () => User | null;
+	export const isLoggedIn: () => boolean;
 }
