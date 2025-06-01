986ad56bbb5a2c81d83e2b79ea8ea4885b2fd852
Pedro Mello <contato@cicatriz.dev>
2025-06-01 17:37:34 -0300

    commit aula 3.2

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
 import type { Pet } from '../../types';
 
-const Card = (pet: Pet) => {
+const Card = ({ pet }: { pet: Pet }) => {
 	return (
 		<div className='bg-[#F6F6F6] rounded-lg shadow-md p-6 w-full flex flex-col items-center justify-center'>
 			<img src={pet.image} alt={pet.name} className='w-40 h-40 object-cover rounded-full' />
